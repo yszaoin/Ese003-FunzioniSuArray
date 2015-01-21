e828aca9722ed372f855a98400bb1a82dbf4c8d6
Francesco <fratrotta@gmail.com>
2015-01-21 20:26:46 +0100

    Corretto tipo del valor medio

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
 CND_ARTIFACT_NAME_Debug=ese003-funzionisuarray
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/ese003-funzionisuarray
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/ese003-funzionisuarray
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Debug=ese003-funzionisuarray.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/ese003-funzionisuarray.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/ese003-funzionisuarray.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
 CND_ARTIFACT_NAME_Release=ese003-funzionisuarray
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/ese003-funzionisuarray
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/ese003-funzionisuarray
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Release=ese003-funzionisuarray.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/ese003-funzionisuarray.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/ese003-funzionisuarray.tar
 #
 # include compiler specific variables
 #
